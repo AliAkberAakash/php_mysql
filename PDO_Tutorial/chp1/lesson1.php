6fd177b2be3cea90947c8d6cb6d98a3764da607a
ALIAKBERAAKASH <ali852609@gmail.com>
2019-12-23 15:52:15 +0600

    finished 23.12.2019

---
+++ PDO_Tutorial/chp1/lesson1.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="myStyle.css">
+</head>
+<body>
+	
+	<form class="center" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		<input  type="submit" name="create_database" value="Create Database">
+	</form>
+
+
+
+
+</body>
+</html>
